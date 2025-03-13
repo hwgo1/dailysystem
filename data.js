20298f1d22f141900c971726d265ec3e941fb638
hwgo <hwgo.henrique1@gmail.com>
2025-03-13 13:26:37 -0300

    feat(home.pug): added empty state to the home
    Added a "create new task" button and "no task added" widget when theres no task on home

--- data.js
+++ data.js
@@ -1,12 +1,3 @@
-const tasks = [
-  {
-    id: 1,
-    name: "Treinar Express.js",
-    exp: 50,
-    difficulty: "D",
-    deadline: "2025-03-15",
-    completed: false,
-  },
-];
+const tasks = [];
 
 export default tasks;
